c3e3ae935c9dd1051c4c37dbd8d0421bd35e1ddf
Mark Zieg <mark@zieg.com>
2015-02-10 13:42:21 +0000

    resolving Maya-LSL branch

---
+++ mvz-lsl/os-support/windows/VisualStudio2010/VSProj/stdafx.cpp
@@ -0,0 +1,6 @@
+// stdafx.cpp : source file that includes just the standard includes
+// SeaBreezeDLL.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "common\SeaBreeze.h"
+#include "stdafx.h"
