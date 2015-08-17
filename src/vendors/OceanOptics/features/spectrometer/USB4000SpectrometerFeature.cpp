804d6eebb989fa1483419ee74f76559f484ad69c
Mark Zieg <mark@zieg.com>
2015-08-17 22:35:23 +0000

    3.0.9: updated EDC pixel indices for USB2000+/Flame-S and USB4000/Flame-T

--- src/vendors/OceanOptics/features/spectrometer/USB4000SpectrometerFeature.cpp
+++ src/vendors/OceanOptics/features/spectrometer/USB4000SpectrometerFeature.cpp
@@ -55,7 +55,9 @@ USB4000SpectrometerFeature::USB4000SpectrometerFeature() {
 	this->integrationTimeBase = USB4000SpectrometerFeature::INTEGRATION_TIME_BASE;
 	this->integrationTimeIncrement = USB4000SpectrometerFeature::INTEGRATION_TIME_INCREMENT;
 
-	for(int i = 2; i < 13; i++) {
+	// safest range for USB4000 and Flame-T is (5, 15)
+	// other branches (5, 16) and (2, 12)
+	for(int i = 5; i < 16; i++) {
 		this->electricDarkPixelIndices.push_back(i);
 	}
 
