c49dd95abe185206ce8a06e5a2c5c34dafae5f70
Aaron Gage <aaron.gage@oceanoptics.com>
2015-11-14 01:54:57 +0000

    [#5] Applying the patch as contributed by Andreas Poehlmann in the tracker.

--- include/api/seabreezeapi/SpectrometerFeatureAdapter.h
+++ include/api/seabreezeapi/SpectrometerFeatureAdapter.h
@@ -64,6 +64,7 @@ class SpectrometerFeatureAdapter: public FeatureAdapterTemplate<OOISpectrometerF
 	void setIntegrationTimeMicros(int *errorCode,
 		unsigned long integrationTimeMicros);
 	long getMinimumIntegrationTimeMicros(int *errorCode);
+	long getMaximumIntensity(int *errorCode);
 };
 
 }// namespace api
