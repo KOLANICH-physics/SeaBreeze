2fd2c0f4cacad0a6285f494ffc65f6b7c48fa119
Aaron Gage <aaron.gage@oceanoptics.com>
2015-11-23 17:10:55 +0000

    Resolving the amg-osx-11 branch.  This was merged (237:238) back to trunk at revision 239.  Issue [#11] will be closed.

---
+++ amg-osx-11/util/PulseGenerator/src/SpectrometerInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PulseGenerator {
+interface SpectrometerInterface {
+	bool init();
+	bool getGPIO(int pin);
+	bool setGPIO(int pin, bool flag);
+}
+}// namespace PulseGenerator
